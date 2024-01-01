7480937be90608d9a08c10b06d95397abff5d379
Carl Jones <c.m.jones001@gmail.com>
2024-01-01 13:22:26 +0100

    Problem 50 optimisation

--- problem_50/src/main.rs
+++ problem_50/src/main.rs
@@ -1,3 +1,4 @@
+use ahash::AHashSet;
 use itertools::Itertools;
 use std::time::Instant;
 use common::get_primes_up_to;
@@ -5,7 +6,7 @@ use common::get_primes_up_to;
 fn main() {
     let limit = 1_000_000;
     let start = Instant::now();
-    let (prime_sum, count) = scan_primes(limit);
+    let (prime_sum, _count) = scan_primes(limit);
     let elapsed = start.elapsed();
 
     let expected = 997651;
@@ -23,30 +24,52 @@ fn main() {
 fn scan_primes(limit: u64) -> (u64, u64) {
     let primes = get_primes_up_to(limit as usize);
     let n_primes = primes.len();
+    let prime_set = primes.iter().copied().collect::<AHashSet<_>>();
 
     let start: usize = 2;
-    (start..n_primes).map(|i| get_series_sum(&primes[i..], limit)).max_by_key(|&(_, count)| count).unwrap()
+    (start..n_primes)
+        .map(|i| get_series_sum(&primes[i..], limit, &prime_set))
+        .max_by_key(|&(_, count)| count)
+        .unwrap()
 }
 
-fn get_series_sum(primes: &[u64], limit: u64) -> (u64, u64) {
+
+/// For the given list of primes, find the sum of the longest series of consecutive primes that
+/// sum to a prime less than the limit, starting with the first element.
+///
+/// Returns the last valid prime sum and number of values in the sequence.
+///
+/// Requires a prime set for fast lookup (1000x) improvement. The AHashSet is even faster than a
+/// Binary search (Vec::binary_search_by_key) for this purpose.
+fn get_series_sum(primes: &[u64], limit: u64, prime_set: &AHashSet<u64>) -> (u64, u64) {
     use itertools::FoldWhile::{Continue, Done};
 
-    let (_, _, last_prime, count) = primes.iter().fold_while((0, 0, 1, 0), |(count, acc_sum, last_prime, last_prime_count), &p| {
-        let new_sum = acc_sum + p;
+    // Iterate over the primes, keeping track of the last prime that was a sum of consecutive primes
+    // and the number of consecutive primes that were summed. We quit when the sum exceeds the limit.
+    let (_, _, count, last_prime) = primes
+        .iter()
+        .fold_while(
+            (0, 0, 0, 0),
+            |(count, acc_sum, last_prime_count, last_prime), &p| {
+                let new_sum = acc_sum + p;
+                let new_count = count + 1;
 
-        let (last_prime, last_prime_count) = if primes.contains(&new_sum) {
-            (new_sum, count + 1)
-        } else {
-            (last_prime, last_prime_count)
-        };
+                // If the new sum is prime, update the last prime and the count
+                let (last_prime, last_prime_count) = if prime_set.contains(&new_sum) {
+                    (new_sum, new_count)
+                } else {
+                    (last_prime, last_prime_count)
+                };
 
-        if new_sum > limit {
-            Done((count, acc_sum, last_prime, last_prime_count))
-        } else {
-            Continue((count + 1, new_sum, last_prime, last_prime_count))
-        }
-
-    }).into_inner();
+                // If the new sum exceeds the limit, we're done
+                if new_sum > limit {
+                    Done((count, acc_sum, last_prime_count, last_prime))
+                } else {
+                    Continue((new_count, new_sum, last_prime_count, last_prime))
+                }
+            },
+        )
+        .into_inner();
 
     (last_prime, count)
 }
@@ -57,10 +80,14 @@ mod test {
 
     #[test]
     fn test_100() {
-        let primes = get_primes_up_to(100).into_iter().skip_while(|&p| p < 2).collect_vec();
+        let primes = get_primes_up_to(100)
+            .into_iter()
+            .skip_while(|&p| p < 2)
+            .collect_vec();
         println!("{:?}", primes);
+        let prime_set = primes.iter().copied().collect::<AHashSet<_>>();
 
-        let (last_prime, count) = get_series_sum(&primes,  100);
+        let (last_prime, count) = get_series_sum(&primes, 100, &prime_set);
         assert_eq!(count, 6);
         assert_eq!(last_prime, 41);
     }
